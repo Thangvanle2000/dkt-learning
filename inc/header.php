d312e600dfe62fc9a630bc2b3ba0ecd42f3301dd
Thangvanle2000 <70746075+Thangvanle2000@users.noreply.github.com>
2021-08-28 00:17:27 +0700

    add dictionary

---
+++ inc/header.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+	<meta name="theme-color" content="#ffffff">
+	<title>AVOS</title>
+
+	<!-- Bootstrap -->
+
+	<link href="css/bootstrap-4.3.1.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+	<link href="css/eCommerceStyle.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+		crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<div name="header-LOGO" style="height: 100px; width: 100%">
+		<div id="logo"><a href="index.php"> <img src="Image/logo/logo 16_9.png" alt="sample logo"></a></div>
+
+	</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top border-bottom border-top border-gray">
+		<a class="navbar-brand" href="#"> <img class="ml-5" src="Image/logo/logo%20small%20trans.png" height="36"
+				alt="">
+		</a>
+		<div class="container ml-3">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img src="Image/icon/courses.svg" width="36px" height="36px" alt="" style="opacity: 50%;">
+							&nbsp; Courses
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="">Course 1</a>
+							<a class="dropdown-item" href="">Course 2</a>
+							<a class="dropdown-item" href="">Course 3</a>
+						</div>
+					</li>
+					<li class="nav-item ml-3">
+
+						<a class="nav-link" href="index.php?page=lib">
+
+
+							<img src="Image/icon/libraries.svg" width="36" height="36" alt="" style="opacity: 50%;">
+
+							&nbsp; Library
+						</a>
+					</li>
+					<li class="nav-item ml-3">
+
+						<a class="nav-link" href="pages/forum.php">
+							<img alt="" fill="#000000" height="36" src="Image/icon/forum.svg" style="opacity:50%;"
+								width="36">
+							&nbsp; Forum
+						</a>
+					</li>
+					<li class="nav-item dropdown ml-3">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img src="Image/icon/more.svg" width="36px" height="36px" alt="" style="opacity: 50%;">
+							&nbsp; More
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="aboutus.php">About us</a>
+							<a class="dropdown-item" href="">News</a>
+							<a class="dropdown-item" href="">Membership</a>
+							<a class="dropdown-item" href="">Donate to Evos</a>
+						</div>
+					</li>
+				</ul>
+
+				<form class="form-inline my-2 my-lg-0 float-right">
+					<button class="opacity-25 btn btn-outline-info btn-toolbar border-3">Login</button>
+					<button class="opacity-25 ml-5 btn btn-outline-info btn-toolbar border-3">Sign up</button>
+					<button class="opacity-25 ml-5 btn btn-outline-info btn-toolbar border-3"><img
+							src="Image/icon/shopping-cart.png" width="25px" height="25px"></button>
+				</form>
+			</div>
+		</div>
+	</nav>
+	<div class="row">
+		<div name="content" class="col-12 ">
+			<nav aria-label="breadcrumb" class= "first">
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item"><a href="#">Home</a></li>
+					<li class="breadcrumb-item"><a href="#">Library</a></li>
+					<li class="breadcrumb-item active">Data</li>
+				</ol>
+			</nav>
+
+
+			</header>
---
+++ inc/menu.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top border-bottom border-top border-gray">
+		<a class="navbar-brand" href="#"> <img class="ml-5" src="Image/logo/logo%20small%20trans.png" height="36"
+				alt="">
+		</a>
+		<div class="container ml-3">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img src="Image/icon/courses.svg" width="36px" height="36px" alt="" style="opacity: 50%;">
+							&nbsp; Courses
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="">Course 1</a>
+							<a class="dropdown-item" href="">Course 2</a>
+							<a class="dropdown-item" href="">Course 3</a>
+						</div>
+					</li>
+					<li class="nav-item ml-3">
+
+						<a class="nav-link" href="index.php?page=lib">
+
+
+							<img src="Image/icon/libraries.svg" width="36" height="36" alt="" style="opacity: 50%;">
+
+							&nbsp; Library
+						</a>
+					</li>
+					<li class="nav-item ml-3">
+
+						<a class="nav-link" href="pages/forum.php">
+							<img alt="" fill="#000000" height="36" src="Image/icon/forum.svg" style="opacity:50%;"
+								width="36">
+							&nbsp; Forum
+						</a>
+					</li>
+					<li class="nav-item dropdown ml-3">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img src="Image/icon/more.svg" width="36px" height="36px" alt="" style="opacity: 50%;">
+							&nbsp; More
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="aboutus.php">About us</a>
+							<a class="dropdown-item" href="">News</a>
+							<a class="dropdown-item" href="">Membership</a>
+							<a class="dropdown-item" href="">Donate to Evos</a>
+						</div>
+					</li>
+				</ul>
+
+				<form class="form-inline my-2 my-lg-0 float-right">
+					<button class="opacity-25 btn btn-outline-info btn-toolbar border-3">Login</button>
+					<button class="opacity-25 ml-5 btn btn-outline-info btn-toolbar border-3">Sign up</button>
+					<button class="opacity-25 ml-5 btn btn-outline-info btn-toolbar border-3"><img
+							src="Image/icon/shopping-cart.png" width="25px" height="25px"></button>
+				</form>
+			</div>
+		</div>
+	</nav>
